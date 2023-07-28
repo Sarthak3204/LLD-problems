6906b9a9d4064a6a0a3f8383f4918003d80650e0
Sarthak <sarthak.vishwakarma.eee21@itbhu.ac.in>
2023-07-28 20:49:34 +0530

    'fixed'

--- LRU_Cache.cpp
+++ LRU_Cache.cpp
@@ -2,17 +2,7 @@
 using namespace std;
 /*
 Design an LRU cache
-
-Constraints and assumptions
-
-What are we caching?
-We are caching the results of web queries
-
-Can we assume inputs are valid or do we have to validate them?
-Assume they're valid
-
-Can we assume this fits memory?
-Yes
+https://leetcode.com/problems/lru-cache/submissions/1006217488/
 */
 class Node {
 public:
